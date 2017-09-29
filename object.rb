909489afc9d129c466a9d406f5c678f75777336a
Tiago Cabral <tiagocabral@cjr.org.br>
2017-09-29 20:56:08 -0300

    Classe GameObject adicionada (pai de Hiero e de Falcon)

--- object.rb
+++ object.rb
@@ -2,14 +2,8 @@ require 'gosu'
 
 TOP_LEVEL = 1 # eixo Z do jogo
 
-class Falcon
+class GameObject
   attr_accessor :x,:y,:vel_x,:vel_y,:angle
-  def initialize
-    @image = Gosu::Image.new("images/falcon.png")
-    @x = @y = @vel_x = @vel_y = 0.0
-    @angle = 0.0
-  end
-
   public
     def warp(x, y)
       @x, @y = x, y
@@ -18,34 +12,36 @@ class Falcon
     def draw
       @image.draw_rot(@x, @y, TOP_LEVEL, @angle)
     end
-    def move_down
-      @x += 2.975
-      @y += 4.375
-    end
-    def move_up
-      @x -= 2.975
-      @y -= 4.375
-    end
 end
 
-class Hiero
-  attr_accessor :x,:y,:vel_x,:vel_y,:angle
+class Falcon < GameObject
+
+  def initialize
+    @image = Gosu::Image.new("images/falcon.png")
+    @x = @y = @vel_x = @vel_y = 0.0
+    @angle = 0.0
+  end
+
+  def move_down
+    @x += 2.975
+    @y += 4.375
+  end
+  def move_up
+    @x -= 2.975
+    @y -= 4.375
+  end
+end
+
+class Hiero < GameObject
+
 	def initialize
     @image = Gosu::Image.new("images/hiero.png")
     @x = @y = @vel_x = @vel_y = 0.0
     @angle = 3.0
-  	end
+  end
 
-  	def warp(x, y)
-      @x, @y = x, y
-    end
-
-  	def draw
-      @image.draw_rot(@x, @y, TOP_LEVEL, @angle)
-    end
-
-    def move
-      @x -= 2
-      @y += 0.7
-    end
+  def move
+    @x -= 2
+    @y += 0.7
+  end
 end
