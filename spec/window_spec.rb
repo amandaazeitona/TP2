28baec5b9b3f1b1185d8256d649bca6d98f46467
arturhbs <arturhenrique71@gmail.com>
2017-11-17 22:45:13 -0200

    acrecentando novos testes

--- spec/window_spec.rb
+++ spec/window_spec.rb
@@ -3,38 +3,42 @@ require 'gosu'
 
 # Aplicando testes na janela do jogo.
 describe GameWindow do
+
   it 'return a new window' do
     window = GameWindow.new
     expect(window).to be_a GameWindow
   end
 
-  it 'move out window right' do
-    falcon = Falcon.new('spec/images/falcon.png')
-    falcon.y = 511
-    eixo_x = falcon.x
-    eixo_y = falcon.y
-    Gosu.button_down? Gosu::KB_RIGHT = 1
-    expect(falcon.x).to eql eixo_x
-    expect(falcon.y).to eql eixo_y
+  it 'access method roda_ranking' do
+  	window = GameWindow.new
+  	window.entrou_ranking = 0
+  	window.roda_ranking
+  	expect(window.entrou_ranking).to eql 1
   end
 
-  it 'move out window left' do
-    falcon = Falcon.new('spec/images/falcon.png')
-    falcon.x = 18
-    eixo_x = falcon.x
-    eixo_y = falcon.y
-    Gosu.button_down? Gosu::KB_LEFT = 1
-    expect(falcon.x).to eql eixo_x
-    expect(falcon.y).to eql eixo_y
-  end
+  #it 'verify draw ' do
+  	#@state = 1
+  	#inicializou = true
+  	#window = GameWindow.new
+   # window.draw
+  #	expect(@inimigos.size).to be > 0
+  #end
 
-  it 'move out window down' do
-    falcon = Falcon.new('spec/images/falcon.png')
-    falcon.x = 488
-    eixo_x = falcon.x
-    eixo_y = falcon.y
-    Gosu.button_down? Gosu::KB_DOWN = 1
-    expect(falcon.x).to eql eixo_x
-    expect(falcon.y).to eql eixo_y
-  end
+  #it 'roda_ranking' do
+  #	window = GameWindow.new
+  #	inicializou = false
+  #	window.roda_ranking
+  #	expect(inicializou).to be true
+  #end
+
+
+#  it 'menu apertando ver ranking' do
+#  	state = 0
+#  	Gosu.button_down? Gosu::KB_R = true
+#  	varl = Gosu.button_down? Gosu::KB_R
+#  	puts varl
+#  	expect(state).to eql 2
+#  end
+
+
 end
