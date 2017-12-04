679e283028512ba442cfeb63964e4a28f3cbd6d8
amandaazeitona <amandaalves@cjr.org.br>
2017-12-04 10:42:03 -0200

    gemfile e simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,9 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-
+require 'simplecov'
+SimpleCov.start
 require_relative '../object'
 require_relative '../window'
+require_relative '../pontuacao'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
